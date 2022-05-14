d69242cd454de99179fca8a7d2b895b93361310a
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-14 15:43:26 -0500

    actualizaciones

--- RepasosBD/Form1.Designer.cs
+++ RepasosBD/Form1.Designer.cs
@@ -29,21 +29,15 @@ namespace RepasosBD
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(22, 21);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(753, 246);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
@@ -54,6 +48,7 @@ namespace RepasosBD
             this.button1.TabIndex = 1;
             this.button1.Text = "Consultar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -64,6 +59,7 @@ namespace RepasosBD
             this.button2.TabIndex = 2;
             this.button2.Text = "Insertar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -74,6 +70,7 @@ namespace RepasosBD
             this.button3.TabIndex = 3;
             this.button3.Text = "Retirar";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -84,34 +81,70 @@ namespace RepasosBD
             this.button4.TabIndex = 4;
             this.button4.Text = "Actualizar";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(753, 247);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(22, 341);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 30);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Ver un Dato";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 347);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(338, 21);
+            this.comboBox1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 346);
+            this.ClientSize = new System.Drawing.Size(800, 391);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- RepasosBD/Form1.cs
+++ RepasosBD/Form1.cs
@@ -8,13 +8,92 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace RepasosBD
 {
     public partial class Form1 : Form
     {
+        private SqlConnection cn;
+
         public Form1()
         {
             InitializeComponent();
+            cn = new SqlConnection("Data Source=(local);Initial Catalog=parainfo;Integrated Security=SSPI;");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM alumnos", cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = cn;
+            cm.CommandText = "INSERT INTO alumnos VALUES('eeeeee', 15, 16, 17)";
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Operación exitosa");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = cn;
+            cm.CommandText = "DELETE FROM alumnos WHERE idalumno=6";
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Operación exitosa");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = cn;
+            cm.CommandText = "UPDATE alumnos SET nombre = 'yyyyyy' WHERE idalumno = 5";
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Operación exitosa");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // CANTIDAD DE FILAS SELECCIONADAS
+            int fils_sel = dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            if(fils_sel > 0)
+            {
+                // TOMAR UN DATO (SEGUNDA COLUMNA) DE LA FILA
+                MessageBox.Show("" + dataGridView1.SelectedRows[0].Cells[1].Value);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione fila");
+            }
+        }
+
+        public void PoblarCombo()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT idalumno, nombre FROM alumnos", cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.ValueMember = "idalumno";
+            comboBox1.DisplayMember = "nombre";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PoblarCombo();
         }
     }
 }
